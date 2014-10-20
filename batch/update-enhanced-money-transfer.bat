69ea9b8699ef4464c12e22a5f8be8271f8f76aad
wysiwyg <andymitchell1504@gmail.com>
2014-10-20 19:49:18 +0100

    Enhanced Money Transfer update v1.07
    Added russian localisation and default language file if no local file
    exists.

--- batch/update-enhanced-money-transfer.bat
+++ batch/update-enhanced-money-transfer.bat
@@ -1,4 +1,4 @@
 echo off
 set ch_note=%1
-workshoptool update -path C:\Users\andy\Dropbox\Games\XRebirth\gitrepo\xrmods\mt_enhanced_money_transfer\packed_release\mt_enhanced_money_transfer -changenote %ch_note%
+workshoptool update -path ..\mt_enhanced_money_transfer\packed_release\mt_enhanced_money_transfer -changenote %ch_note%
 echo %ch_note%
