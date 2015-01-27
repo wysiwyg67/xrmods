c88f4d979f5df866dcac2f5fbda841d8bdd68c2f
wysiwyg <andymitchell1504@gmail.com>
2015-01-27 15:29:57 +0000

    EMT Update 1.11
    Added french translation + fix for XR 3.20 RC related UI changes

--- mt_enhanced_money_transfer/build/publish_emt.bat
+++ mt_enhanced_money_transfer/build/publish_emt.bat
@@ -6,6 +6,7 @@ REM 2014/12/04
 REM if no parameter then update else first time publish 
 IF "%1" EQU "update" GOTO Update_mod
 IF "%1" EQU "publish" GOTO Publish_mod
+IF "%1" EQU "minor" GOTO Update_minor
 GOTO Error_args
 
 :Publish_mod
@@ -20,6 +21,15 @@ IF "%~3" EQU "" GOTO Error_args
 ECHO %change_note%
 WorkshopTool update -path "..\release\%version%\mt_enhanced_money_transfer" -changenote %change_note%
 GOTO End_of_prog
+
+:Update_minor
+set change_note="%~3"
+set version=%2
+IF "%~3" EQU "" GOTO Error_args
+ECHO %change_note%
+WorkshopTool update -minor -path "..\release\%version%\mt_enhanced_money_transfer" -changenote %change_note%
+GOTO End_of_prog
+
 
 :Error_args
 ECHO "Incorrect command line arguments....."
